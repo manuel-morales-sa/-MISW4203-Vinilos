2cf9fe2f933ea54f5ad5c3a3876ad07641ddede4
Manuel Morales <mf.moraless1@uniandes.edu.co>
2022-11-04 01:35:16 -0500

    format 3

--- app/src/main/java/com/example/vinilos/ui/main/view/HomeActivity.kt
+++ app/src/main/java/com/example/vinilos/ui/main/view/HomeActivity.kt
@@ -1,51 +1,33 @@
 package com.example.vinilos.ui.main.view
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.vinilos.ui.main.adapter.MainAdapter
-import com.example.vinilos.ui.main.viewmodel.MainViewModel
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import android.view.View
-import android.widget.Toast
 import com.example.vinilos.data.api.ApiHelper
 import com.example.vinilos.data.api.RetrofitBuilder
 import com.example.vinilos.data.model.albumResponse
-import com.vinylsMobile.vinylsapplication.databinding.ActivityMainBinding
 import com.example.vinilos.ui.base.ViewModelFactory
+import com.example.vinilos.ui.main.adapter.MainAdapter
+import com.example.vinilos.ui.main.viewmodel.MainViewModel
+import com.vinylsMobile.vinylsapplication.R
+import com.vinylsMobile.vinylsapplication.databinding.ActivityMainBinding
 import com.vinylsMobile.vinylsapplication.utils.Status
 
-
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.collectorButton.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.visitorButton.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        }
-
-    }
-}
-    /*private lateinit var mainViewModel: MainViewModel
+class HomeActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainViewModel
     private lateinit var adapter: MainAdapter
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         setupUI()
@@ -70,18 +52,18 @@ class MainActivity : AppCompatActivity() {
             it?.let { resource ->
                 when (resource.status) {
                     Status.SUCCESS -> {
-                        binding.recyclerView.visibility = View.VISIBLE
+                        //binding.recyclerView.visibility = View.VISIBLE
                         //binding.progressBar.visibility = View.GONE
                         resource.data?.let { albums -> retrieveList(albums) }
                     }
                     Status.ERROR-> {
-                        binding.recyclerView.visibility = View.VISIBLE
+                        //binding.recyclerView.visibility = View.VISIBLE
                         //binding.progressBar.visibility = View.GONE
                         Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
                     }
                     Status.LOADING -> {
                         //binding.progressBar.visibility = View.VISIBLE
-                        binding.recyclerView.visibility = View.GONE
+                        //binding.recyclerView.visibility = View.GONE
                     }
                 }
             }
@@ -102,4 +84,4 @@ class MainActivity : AppCompatActivity() {
             notifyDataSetChanged()
         }
     }
-}*/
+}
