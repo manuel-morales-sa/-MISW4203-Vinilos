e49ae59b7340aa9b4dc709d8ed0da2397874478b
Manuel Morales <mf.moraless1@uniandes.edu.co>
2022-11-04 01:20:59 -0500

    format 2

--- app/src/main/java/com/example/vinilos/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/ui/main/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vinilos.ui.main.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vinilos.ui.main.adapter.MainAdapter
@@ -19,7 +20,26 @@ import com.vinylsMobile.vinylsapplication.utils.Status
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainViewModel: MainViewModel
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.collectorButton.setOnClickListener {
+            //val intent = Intent(this, AlbumsListActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.visitorButton.setOnClickListener {
+            //val intent = Intent(this, AlbumsListActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
+    /*private lateinit var mainViewModel: MainViewModel
     private lateinit var adapter: MainAdapter
     private lateinit var binding: ActivityMainBinding
 
@@ -51,16 +71,16 @@ class MainActivity : AppCompatActivity() {
                 when (resource.status) {
                     Status.SUCCESS -> {
                         binding.recyclerView.visibility = View.VISIBLE
-                        binding.progressBar.visibility = View.GONE
+                        //binding.progressBar.visibility = View.GONE
                         resource.data?.let { albums -> retrieveList(albums) }
                     }
                     Status.ERROR-> {
                         binding.recyclerView.visibility = View.VISIBLE
-                        binding.progressBar.visibility = View.GONE
+                        //binding.progressBar.visibility = View.GONE
                         Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
                     }
                     Status.LOADING -> {
-                        binding.progressBar.visibility = View.VISIBLE
+                        //binding.progressBar.visibility = View.VISIBLE
                         binding.recyclerView.visibility = View.GONE
                     }
                 }
@@ -82,4 +102,4 @@ class MainActivity : AppCompatActivity() {
             notifyDataSetChanged()
         }
     }
-}
+}*/
