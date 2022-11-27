9e48a37d39f1c306245ed3c1a678d64cd3c113f1
Jose Alexander Palacio Muñoz <j.palaciom@uniandes.edu.co>
2022-11-27 16:15:58 -0500

    Fixing album description BUG05

--- app/src/main/java/com/example/vinilos/ui/main/adapter/DetailAdapter.kt
+++ app/src/main/java/com/example/vinilos/ui/main/adapter/DetailAdapter.kt
@@ -14,6 +14,7 @@ class DetailAdapter(private val albumDetail: AlbumResponse) {
         Glide.with(binding.imageViewAlbumDetails.context)
             .load(albumDetail.cover)
             .into(binding.imageViewAlbumDetails)
+        binding.textContentAlbum.text = albumDetail.name
         binding.textContentDate.text = formatDate(albumDetail.releaseDate)
         binding.textContentGenre.text = albumDetail.genre
         binding.textContentRecord.text = albumDetail.recordLabel
